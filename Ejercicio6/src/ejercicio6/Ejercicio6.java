c5b4484121c42b63df5c3a690cb3f550bab34de6
Daniel Castellanos <dfelipecast2597@hotmail.com>
2016-02-01 13:20:13 -0500

    Modificacion de la clase principal y creacion de la clase kamasutra

--- Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -15,7 +15,10 @@ public class Ejercicio6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Daniel Felipe Csstellanos");
+        Kamasutra objeto = new Kamasutra();
+        objeto.setPosicion("El helipcote en aterrizaje");
+        System.out.println(objeto.getPosicion());
     }
     
 }
